d04822d789380965b31699b62afe8fa81e1472b8
yinyanlv <yanlv.yin@gmail.com>
2017-06-23 16:02:15 +0800

    删除升级命令脚本

--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-npm install @angular/animations@latest  @angular/common@latest  @angular/compiler@latest  @angular/core@latest  @angular/forms@latest  @angular/http@latest  @angular/platform-browser@latest  @angular/platform-browser-dynamic@latest  @angular/router@latest core-js@latest font-awesome@latest jquery@1.11.3  ng2-translate@latest  primeng@latest  rxjs@latest zone.js@latest   --save
-
-npm install @angular/cli@latest @angular/compiler-cli@latest  @types/jasmine@latest  @types/node@latest angular-in-memory-web-api@latest  codelyzer@latest  jasmine-core@latest  jasmine-spec-reporter@latest  karma@latest  karma-chrome-launcher@latest  karma-cli@latest  karma-jasmine@latest  karma-remap-istanbul@latest  protractor@latest  ts-node@latest  tslint@latest  typescript@latest   ts-helpers@latest  --save-dev
